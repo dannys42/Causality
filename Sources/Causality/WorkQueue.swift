757c3feaeadd23f67600a0339d7a9e188510d8e8
Danny Sung <danny@dannysung.com>
2020-09-20 20:04:13 -0700

    WorkQueue: update equatable test for Linux

--- Sources/Causality/WorkQueue.swift
+++ Sources/Causality/WorkQueue.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-internal enum WorkQueue: Equatable {
+internal enum WorkQueue {
     case none
     case dispatch(DispatchQueue)
     case operation(OperationQueue)
@@ -26,6 +26,22 @@ extension WorkQueue {
             q.addOperation {
                 work()
             }
+        }
+    }
+}
+
+extension WorkQueue: Equatable {
+    static func ==(lhs: WorkQueue, rhs: WorkQueue) -> Bool {
+        switch (lhs,rhs) {
+        case (.none, .none):
+            return true
+        case (.dispatch(let q1), .dispatch(let q2)):
+            // this is the only comparison available for Linux for DispatchQueue's
+            return q1 === q2
+        case (.operation(let q1), .operation(let q2)):
+            return q1 == q2
+        default:
+            return false
         }
     }
 }
